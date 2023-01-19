5c483405998530bccd5b834cd55d6108e3722edc
Joaquin Viera <joaquin.viera@outlook.com>
2023-01-19 19:47:49 -0300

    Task model

--- src/pages/api/tasks/index.js
+++ src/pages/api/tasks/index.js
@@ -1,7 +1,9 @@
+import Tasks from "../../../models/Tasks"
 import { dbConnect } from "../../../utils/mongoose"
-
 dbConnect()
 
-export default function handler (req, res) {
+export default async function handler (req, res) {
+  const tasks = await Tasks.find()
+  console.log(tasks)
   res.status(200).json("tasks")
 }
