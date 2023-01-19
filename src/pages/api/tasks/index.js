8d1bf8b12f58f53c8c3bee2b03d25054b0db4c15
Joaquin Viera <joaquin.viera@outlook.com>
2023-01-19 20:28:04 -0300

    get and create tasks

--- src/pages/api/tasks/index.js
+++ src/pages/api/tasks/index.js
@@ -1,9 +1,29 @@
-import Tasks from "models/Tasks"
+/* eslint-disable no-case-declarations */
+import Task from "models/Tasks"
 import { dbConnect } from "utils/mongoose"
 dbConnect()
 
 export default async function handler (req, res) {
-  const tasks = await Tasks.find()
-  console.log(tasks)
-  res.status(200).json("tasks")
+  const { method, body } = req
+  switch (method) {
+  case "GET":
+    try {
+      const tasks = await Task.find()
+      return res.status(200).json(tasks)
+    } catch (error) {
+      return res.status(500).json(error)
+    }
+
+  case "POST":
+    try {
+      const newTask = new Task(body)
+      const savedTask = await newTask.save()
+      return res.status(201).json(savedTask)
+    } catch (error) {
+      return res.status(500).json(error)
+    }
+
+  default:
+    return res.status("400").json({ msg: "Method do not allowed" })
+  }
 }
