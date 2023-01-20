a8566eef320004a564870e8ae2d8c622d1663089
Joaquin Viera <joaquin.viera@outlook.com>
2023-01-20 12:05:08 -0300

    typo fix

--- src/utils/mongoose.js
+++ src/utils/mongoose.js
@@ -1,14 +1,14 @@
 import { connect, connection } from "mongoose"
 
 const conn = {
-  isConnect: false
+  isConnected: false
 }
 
 export async function dbConnect () {
-  if (conn.isConnect) return
+  if (conn.isConnected) return
   const db = await connect(process.env.MONGODB_URL)
 
-  conn.isConnect = db.connections[0].readyState
+  conn.isConnected = db.connections[0].readyState
   console.log(db.connection.db.databaseName)
 }
 
