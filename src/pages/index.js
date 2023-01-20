73c096c47b5138a00474f9296c4f9539eaa4dbf8
Joaquin Viera <joaquin.viera@outlook.com>
2023-01-20 12:33:57 -0300

    semantic ui added

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,3 +5,13 @@ export default function HomePage () {
     <div>Hello World</div>
   )
 }
+
+export async function getServerSideProps (context) {
+  const tasks = await fetch("http://localhost:3000/api/tasks")
+  const data = await tasks.json()
+
+  console.log(data)
+  return {
+    props: {}
+  }
+}
