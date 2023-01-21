3985c172996bef4a5deade11425546da71e8379d
Joaquin Viera <joaquin.viera@outlook.com>
2023-01-21 15:50:13 -0300

    create task look and feel

--- src/pages/tasks/new.js
+++ src/pages/tasks/new.js
@@ -1,7 +1,28 @@
 import React from "react"
+import { Button, Form, Grid } from "semantic-ui-react"
 
 export default function TaskFormPage () {
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log("submit")
+  }
   return (
-    <div>New Task</div>
+    <Grid
+      centered
+      verticalAlign="middle"
+      columns={3}
+      style={{ height: "80vh" }}
+    >
+      <Grid.Row>
+        <Grid.Column textAlign="center">
+          <h1>Create Task</h1>
+          <Form onSubmit={handleSubmit}>
+            <Form.Input label="Title" placeholder="Title" />
+            <Form.TextArea label="Description" placeholder="Description" />
+            <Button primary>Save</Button>
+          </Form>
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
   )
 }
