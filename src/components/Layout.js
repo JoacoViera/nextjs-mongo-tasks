559df55e67b085467b33635aaa871e3ce186b803
Joaquin Viera <joaquin.viera@outlook.com>
2023-01-21 15:37:27 -0300

    navigation

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,6 @@
 import { Navbar } from "./Navbar"
 
 export const Layout = (props) => {
-  console.log("ke", props)
   return (
     <>
       <Navbar />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,22 @@
 
+import Link from "next/link"
+import { useRouter } from "next/router"
 import React from "react"
 import { Button, Menu, Container } from "semantic-ui-react"
 
 export const Navbar = () => {
+  const router = useRouter()
   return (
     <Menu inverted borderless attached>
       <Container>
         <Menu.Item>
-          <img src="./favicon.ico"></img>
+          <Link href={"/"}>
+            <img src="./favicon.ico"></img>
+          </Link>
         </Menu.Item>
         <Menu.Menu position="right">
           <Menu.Item>
-            <Button primary size="mini" onClick={() => alert("click")}>
+            <Button primary size="mini" onClick={() => router.push("/tasks/new")}>
               New Task
             </Button>
           </Menu.Item>
